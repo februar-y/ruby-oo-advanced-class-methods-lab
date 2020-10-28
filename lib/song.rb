c7cd6498bbbb8bef97816e27978db4f46b96f035
februar-y <jarmah241@gmail.com>
2020-10-27 21:56:24 -0400

    completed!!

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|object| object.name}
+  end
+
+
+  # Regex OP
+  def self.new_from_filename(file)
+    song = self.new
+    name_art = file.split(/\ - |\.mp3/)
+    song.name = name_art[1]
+    song.artist_name = name_art[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new
+    name_art = file.split(/\ - |\.mp3/)
+    song.name = name_art[1]
+    song.artist_name = name_art[0]
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    # self.all.clear
+    @@all.clear
+  end
+
 end
+
